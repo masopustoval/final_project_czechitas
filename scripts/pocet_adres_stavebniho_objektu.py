5faf50befefc5d179a20653d20fa4ad4eee2f199
Lucie Masopustová <64306836+masopustoval@users.noreply.github.com>
2020-06-13 22:43:17 +0200

    zjednoduseni kodu

--- scripts/pocet_adres_stavebniho_objektu.py
+++ scripts/pocet_adres_stavebniho_objektu.py
@@ -14,18 +14,15 @@ objekt = collections.defaultdict(list)
 for zpusob_vyuziti_kod in range(1,31):
     cur.execute("SELECT so.kod, cislo_domovni FROM rn_stavebni_objekt so JOIN rn_adresni_misto a ON so.kod = a.stavobj_kod WHERE so.zpusob_vyuziti_kod = {}".format(zpusob_vyuziti_kod))
     adresni_mista = cur.fetchall()
-    x = 0
     for zaznam in adresni_mista:
-        adresy = []
-        kod_objektu = adresni_mista[x][0]
+        kod_objektu = zaznam[0]
         kod_objektu = str(kod_objektu)
-        adresni_misto = adresni_mista[x][1]
+        adresni_misto = zaznam[1]
         adresni_misto = str(adresni_misto)
         if kod_objektu in objekt:
             objekt[kod_objektu].append(adresni_misto)
         else:
             objekt[kod_objektu] = [adresni_misto]
-        x += 1
 
 for kod, pocet_adres in objekt.items():
     conn_insert = psycopg2.connect(host="172.18.0.2", dbname="ruian", user="ruianuser", password='123456')
